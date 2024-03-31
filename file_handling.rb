2bf4cbaff18bf3ba2c34c01dc9f280d4b12b8ce4
minhan6559 <minhan6559@gmail.com>
2024-03-31 23:08:31 +1100

    add dataset_bin

---
+++ file_handling.rb
@@ -0,0 +1,35 @@
+require 'numo/narray'
+require 'polars-df'
+
+x_train = Polars.read_csv("./dataset/30000_X_train.csv").to_numo
+y_train = Polars.read_csv("./dataset/30000_Y_train.csv").to_numo
+
+fp = File.open("./dataset_bin/30000_X_train.bin", "wb")
+fp.write(Marshal.dump(x_train))
+fp.close
+
+fp = File.open("./dataset_bin/30000_Y_train.bin", "wb")
+fp.write(Marshal.dump(y_train))
+fp.close
+
+x_train = Polars.read_csv("./dataset/full_X_train.csv").to_numo
+y_train = Polars.read_csv("./dataset/full_Y_train.csv").to_numo
+
+fp = File.open("./dataset_bin/full_X_train.bin", "wb")
+fp.write(Marshal.dump(x_train))
+fp.close
+
+fp = File.open("./dataset_bin/full_Y_train.bin", "wb")
+fp.write(Marshal.dump(y_train))
+fp.close
+
+x_val = Polars.read_csv("./dataset/X_val.csv").to_numo
+y_val = Polars.read_csv("./dataset/Y_val.csv").to_numo
+
+fp = File.open("./dataset_bin/X_val.bin", "wb")
+fp.write(Marshal.dump(x_val))
+fp.close
+
+fp = File.open("./dataset_bin/Y_val.bin", "wb")
+fp.write(Marshal.dump(y_val))
+fp.close
