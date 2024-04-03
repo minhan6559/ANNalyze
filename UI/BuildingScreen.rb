0533b30532876ac410cd3409d511e32b3a6d4a8b
minhan6559 <minhan6559@gmail.com>
2024-04-03 11:00:40 +1100

    fix file path

--- UI/BuildingScreen.rb
+++ UI/BuildingScreen.rb
@@ -7,17 +7,17 @@ def render_building_screen(events)
     events = remove_all_events(events)
     
     main_menu_background = Image.new(
-        'images\BuildingScreen\Building_screen_Background.png',
+        './images/BuildingScreen/Building_screen_background.png',
         x: 0, y: 0
     )
 
     events << create_button(
-        'images\BuildingScreen\Start_button.png',
+        './images/BuildingScreen/Start_button.png',
         981, 610, 133, 58
     )
 
     events << create_button(
-        'images\BuildingScreen\Home_button.png',
-        1171, 12, 55, 47
+        './images/BuildingScreen/Home_button.png',
+        1171, 10, 55, 47
     )
 end
