5f271e3a3188c22dc8bf1f54fe2230e65e3bbe55
minhan6559 <minhan6559@gmail.com>
2024-09-03 15:41:47 +1000

    Reapply "add cmt main"
    This reverts commit 010e5b2e5d109a2ccf5a297e8aebc677e45b4c2b.

--- main.rb
+++ main.rb
@@ -1,3 +1,6 @@
+# This file is the entry point of the program.
+# It requires the Model class from the ANN module and the ScreenEngine class from the UI module.
+# It then calls the start_window method to start the program.
 require_relative './lib/ANN/Model.rb'
 require_relative './lib/UI/ScreenEngine.rb'
 
